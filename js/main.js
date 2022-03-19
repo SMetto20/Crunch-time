229d9170460997ed05dcef55eb91fefd16158da3
smetto20 <sandra.metto@student.moringaschool.com>
2022-03-19 13:07:47 +0300

    adds html elements to java script

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var form = document.getElementById("form")
+var size = document.getElementById("size")
+var crust = document.getElementById("crust")
+var toppings = document.getElementById("toppings")
+var form = document.getElementById("form")
+var quantity = document.getElementById("quantity")
+var delivery= document.getElementById("delivery")
+var margherita = document.getElementById("margherita")
+var hawaian = document.getElementById("hawaian")
+var nyamachoma = document.getElementById("nyamachoma")
+var vegeterian = document.getElementById("veggie")
+
+
+margherita.addEventListener("click", function () {
+    
+})
+
+hawaian.addEventListener("click", function () {
+    
+})
+
+nyamachoma.addEventListener("click", function () {
+    
+})
+vegeterian.addEventListener("click", function () {
+    
+})
+
