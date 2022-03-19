82315cdc5e552ae1e9acc311dd66f341664dbfd6
smetto20 <sandra.metto@student.moringaschool.com>
2022-03-19 15:09:47 +0300

    adds styling and proper spacing to menu page

--- js/main.js
+++ js/main.js
@@ -9,9 +9,12 @@ var margherita = document.getElementById("margherita")
 var hawaian = document.getElementById("hawaian")
 var nyamachoma = document.getElementById("nyamachoma")
 var vegeterian = document.getElementById("veggie")
-
+var size =document.getElementById("size")
+var crust =document.getElementById("crust")
+var submit=document.getElementById("submit")
 
 margherita.addEventListener("click", function () {
+    
     
 })
 
@@ -25,4 +28,19 @@ nyamachoma.addEventListener("click", function () {
 vegeterian.addEventListener("click", function () {
     
 })
+ 
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    priceCheck()
+})
 
+function priceCheck(){
+    if(size.value =="large" && crust.value =="thick"){
+        console.log(950+8)
+       }
+}
+
+// let pizzamarghe1 ={
+//     pizazsize:size[1]
+    
+// }
